580c566dac389b8376d552bfab7b94bebd9bc800
Your Name <you@example.com>
2023-10-07 13:37:10 +0100

    Second Version

--- src/main/java/com/example/spring_cloud_kafka_stream/services/PageEventService.java
+++ src/main/java/com/example/spring_cloud_kafka_stream/services/PageEventService.java
@@ -2,9 +2,13 @@ package com.example.spring_cloud_kafka_stream.services;
 
 
 import com.example.spring_cloud_kafka_stream.entities.PageEvent;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -39,7 +43,7 @@ public class PageEventService {
 
     }
 
-    // producer + consumer
+    // producer(out) + consumer(in)
 
     @Bean
     public Function<PageEvent,PageEvent> pageEventFunction(){
@@ -50,6 +54,24 @@ public class PageEventService {
         };
     }
 
+
+public Function<KStream<String,PageEvent>,KStream<String,Long>>  kStreamFunction(){
+
+        return (input)->{
+
+        return input
+                .filter((k,v)->v.getDuration()>100)
+                .map((k,v)->new KeyValue<>(v.getName(),0l))
+                .groupBy((k,v)->k,Grouped.with(Serdes.String(),Serdes.Long()))
+                .count()
+                .toStream();
+
+
+ };
+
+
+
+}
 
 
 
